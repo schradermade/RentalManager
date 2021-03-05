d6d60ea11d11354d19cd1147cac14b080c01cc04
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 14:21:46 -0800

    add imports,  const, export

---
+++ components/HomeItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomeItem = (props) => {
+  return (
+    <View>
+      <Text>component</Text>
+    </View>
+  );
+};
+
+export default HomeItem;
