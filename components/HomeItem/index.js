42494098e5db33559389e44a3280d397f4378b66
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 14:52:16 -0800

    ainsert StyledButton component

--- components/HomeItem/index.js
+++ components/HomeItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
-import styles from './styles';
+import styles from './style';
+import StyleButton from '../StyledButton';
 
 const HomeItem = (props) => {
   return (
@@ -8,13 +9,17 @@ const HomeItem = (props) => {
       <View style={styles.homeContainer}>
         {/* render background image */}
         <ImageBackground source={require('../../assets/images/cherry.jpeg')}
-        style={styles.image}></ImageBackground>
+        style={styles.image}
+        />
+
         {/* render titles and subtitles */}
         <View style={styles.titles}>
           <Text style={styles.title}>12345 SW Main St</Text>
           <Text style={styles.subtitle}>3 bed, 2 bath, 1,500sqft</Text>
           <Text style={styles.subtitle}>$1,995/month</Text>
         </View>
+
+        <StyleButton />
       </View>
     </View>
   );
