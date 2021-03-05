54da7872a81ac4230f50d5c35bd228b5a0036858
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 15:04:52 -0800

    add Pressable attribute, Pressable tag to build out button

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import styles from './styles';
 
 const StyleButton = (props) => {
   return (
     <View>
-      <Text></Text>
+      <View>
+        <Pressable
+        style={styles.button}
+        onPress={() => {
+          console.warn('Im down here!')
+        }}
+        >
+          <Text>House Avail!</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
