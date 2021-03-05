571035919092ccbccf4c41961e9d27ad0ebd8866
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 15:28:07 -0800

    add container, button styling, text styling

--- components/StyledButton/styles.js
+++ components/StyledButton/styles.js
@@ -1,7 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    // render as full width. any width of browser, it will take that space
+    width: '100%',
+    padding: 10,
+  },
+  button: {
+    backgroundColor: 'white',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25,
+  },
+  text: {
 
+  }
 });
 
 export default styles;
