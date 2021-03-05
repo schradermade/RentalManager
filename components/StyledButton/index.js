7c808762a62266516ff6c3237cf931bd1fbf967e
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 15:42:45 -0800

    add dynamica color of button

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -3,16 +3,23 @@ import { View, Text, Pressable } from 'react-native';
 import styles from './styles';
 
 const StyleButton = (props) => {
+
+  const type = props.type;
+
+  const backgroundColor = type === 'primary' ? 'grey' : 'white';
+  const textColor = type === 'primary' ? 'white' : 'grey';
+
+  console.warn(type);
   return (
     <View>
       <View style={styles.container}>
         <Pressable
-        style={styles.button}
+        style={[styles.button, {backgroundColor: backgroundColor}]}
         onPress={() => {
           console.warn('Im down here!')
         }}
         >
-          <Text style={styles.text}>House Avail!</Text>
+          <Text style={[styles.text, {color: textColor}]}>House Avail!</Text>
         </Pressable>
       </View>
     </View>
