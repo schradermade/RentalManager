356a04ae06d7b341a79ac290900c62bb6aa01d54
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 14:29:19 -0800

    iadd import styles

--- components/HomeItem/index.js
+++ components/HomeItem/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
+import styles from './styles';
 
 const HomeItem = (props) => {
   return (
     <View>
-      <Text>component</Text>
+      <View style={styles.homeContainer}>
+        {/* render background image */}
+        <ImageBackground source={require('../../assets/images/cherry.jpeg')}
+        style={styles.image}></ImageBackground>
+        {/* render titles and subtitles */}
+        <View style={styles.titles}>
+          <Text style={styles.title}>12345 SW Main St</Text>
+          <Text style={styles.subtitle}>3 bed, 2 bath, 1,500sqft</Text>
+          <Text style={styles.subtitle}>$1,995/month</Text>
+        </View>
+      </View>
     </View>
   );
 };
