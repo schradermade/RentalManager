780f2d8335f1ea1b2b1cf84672a12df3cdf09ef0
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 14:28:51 -0800

    transfer styleing to styles.js

--- components/HomeItem/styles.js
+++ components/HomeItem/styles.js
@@ -2,7 +2,31 @@ import { StyleSheet } from 'react-native';
 
 //declaring styles using StyleSheet.create
 const styles = StyleSheet.create({
-
+  homeContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles: {
+    marginTop: '35%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: '400',
+  },
+  subtitle: {
+    fontSize: 20,
+    color: 'grey',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    //reside mode indicates how pic will resize if does not fit on whole screen
+    resizeMode: 'cover',
+    // position: absolute, so it doesnt push our component underneath
+    position: 'absolute',
+  }
 });
 
 export default styles;
