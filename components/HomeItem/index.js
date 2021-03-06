60f36be2494742d7af1867d7edf44a6952aa617a
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2021-03-05 16:16:22 -0800

    move StyledButton components inside View tag, in order to add styling

--- components/HomeItem/index.js
+++ components/HomeItem/index.js
@@ -18,22 +18,26 @@ const HomeItem = (props) => {
           <Text style={styles.subtitle}>3 bed, 2 bath, 1,500sqft</Text>
           <Text style={styles.subtitle}>$1,995/month</Text>
         </View>
+        
+        <View style={styles.buttonsContainer}>
+          <StyledButton 
+          type='primary' 
+          content={'Get More Info'}
+          onPress={() => {
+            console.warn("Get More Info was pressed");
+          }}
+          />
+          
+          {/* onPress is passed as a prop to StyledButton component for dynamic insert of function */}
+          <StyledButton 
+          type='secondary' 
+          content={'Not interested'}
+          onPress={() => {
+            console.warn("Not interested was pressed");
+          }}
+          />
+        </View>
 
-        <StyledButton 
-        type='primary' 
-        content={'Get More Info'}
-        onPress={() => {
-          console.warn("Get More Info was pressed");
-        }}
-        />
-
-        <StyledButton 
-        type='secondary' 
-        content={'Not interested'}
-        onPress={() => {
-          console.warn("Not interested was pressed");
-        }}
-        />
 
 
       </View>
